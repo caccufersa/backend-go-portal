b325de287a980a617ff4a0c508d55616ee9d0d33
robertfabio <fabio.filho25453@alunos.ufersa.edu.br>
2026-02-11 10:22:08 -0300

    feat(cors): add X-API-Key to allowed headers in CORS configuration

--- pkg/middleware/cors.go
+++ pkg/middleware/cors.go
@@ -8,7 +8,7 @@ func CORSConfig() cors.Config {
 	return cors.Config{
 		AllowOrigins:     "http://localhost:3000,https://portal-cacc-frontend.vercel.app,https://localhost:5173",
 		AllowMethods:     "POST,GET,DELETE,PUT,OPTIONS",
-		AllowHeaders:     "Content-Type,Cache-Control,Pragma,Authorization",
+		AllowHeaders:     "Content-Type,Cache-Control,Pragma,Authorization,X-API-Key",
 		AllowCredentials: true,
 	}
 }
