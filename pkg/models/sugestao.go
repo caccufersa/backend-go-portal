5bc85c7453a4b27d4f80cc89c3d38f5906c534a3
robertfabio <fabio.filho25453@alunos.ufersa.edu.br>
2026-02-18 15:07:01 -0300

    fix: correct JSON tag for CreatedAt field in Sugestao model

--- pkg/models/sugestao.go
+++ pkg/models/sugestao.go
@@ -5,7 +5,7 @@ import "time"
 type Sugestao struct {
 	ID        int       `json:"id"`
 	Texto     string    `json:"texto"`
-	CreatedAt time.Time `json:"data_criacao"`
+	CreatedAt time.Time `json:"created_at"`
 	Author    string    `json:"author"`
 	Categoria string    `json:"categoria"`
 }
